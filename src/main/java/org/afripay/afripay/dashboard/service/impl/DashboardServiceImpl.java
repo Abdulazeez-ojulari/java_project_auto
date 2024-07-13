96e3e194a1eee68259c9abe0940990f36950bf21
Abdulazeez Ojulari <jlrabdulazeez@gmail.com>
2024-07-13 10:29:37 +0100

    test 2

--- src/main/java/org/afripay/afripay/dashboard/service/impl/DashboardServiceImpl.java
+++ src/main/java/org/afripay/afripay/dashboard/service/impl/DashboardServiceImpl.java
@@ -1,12 +1,10 @@
 package org.afripay.afripay.dashboard.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.afripay.afripay.dashboard.service.DashboardService;
 import org.afripay.afripay.transactions.service.TransactionService;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
